72155659814f4dd6ddb1a21371c8fa0031c0a18c
Faizan <faizanamjad575@gmail.com>
2025-07-21 16:14:42 +0500

    implement the welcome screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import WelcomeScreen from "./components/welcome-screen";
 import "./App.css";
 
 function App() {
-    return <></>;
+    const [currentScreen, setCurrentScreen] = useState("welcome"); // To keep track of which screen to render
+
+    if (currentScreen === "welcome")
+        return <WelcomeScreen changeScreen={setCurrentScreen} />;
 }
 
 export default App;
