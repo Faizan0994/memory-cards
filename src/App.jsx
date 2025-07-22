abcb23e44a81ebfd4a5cc4cd58118403f51b0aa3
Faizan <faizanamjad575@gmail.com>
2025-07-22 13:09:41 +0500

    implement the loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import WelcomeScreen from "./components/welcome-screen";
+import Loading from "./components/load-screen";
+import loadCards from "./cards";
 import "./App.css";
 
 function App() {
     const [currentScreen, setCurrentScreen] = useState("welcome"); // To keep track of which screen to render
+    const [cardsData, setCardsData] = useState({}); // To keep cards data
 
     if (currentScreen === "welcome")
         return <WelcomeScreen changeScreen={setCurrentScreen} />;
+    else if (currentScreen === "loading")
+        return (
+            <Loading
+                load={loadCards}
+                changeScreen={setCurrentScreen}
+                returnData={setCardsData}
+            />
+        );
 }
 
 export default App;
